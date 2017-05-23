ee68bad2a4f6d94c7638bbd50b42d2fe566e70c3
Chiranth7954 <ckishore@purdue.edu>
2017-05-23 14:36:54 -0400

    Add comment box and button from class

---
+++ src/Main_Comments.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Main_Comments extends Component {
+  render() {
+    return (
+      <div>
+        <textarea></textarea>
+        <button className="button">Submit comment!</button>
+      </div>
+    )
+  }
+}
+
+export default Main_Comments
--- src/Main_Links.js
+++ src/Main_Links.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import Main_Comments from './Main_Comments'
 import './Main_Links.css'
 
 class Main_Links extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showComments : false,
+    }
+
+    this.toggleComments = this.toggleComments.bind(this)
+  }
+
+  toggleComments(ev) {
+    ev.preventDefault()
+    this.setState({
+      showComments : !this.state.showComments,
+    })
+  }
+
   render() {
     return (
       <div className="article-links">
-        <a className="article-link" href="#">
+        <a className="article-link" href="#" onClick={this.toggleComments}>
           <i className="fa fa-comments-o"></i>
           <span className="article-link-text">Comments</span>
         </a>
@@ -13,6 +30,7 @@ class Main_Links extends Component {
           <i className="fa fa-share"></i>
           <span className="article-link-text">Share Post</span>
         </a>
+        {this.state.showComments ? <Main_Comments /> : null}
       </div>
     )
   }
