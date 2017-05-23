ffe188ccbe7e223f9636ccf49b77e86e92f7ef95
Chiranth7954 <ckishore@purdue.edu>
2017-05-23 15:03:20 -0400

    Add comment functionality from class

--- src/Main_Comments.js
+++ src/Main_Comments.js
@@ -1,29 +1,52 @@
 import React, { Component } from 'react'
+import './Main_Comments.css'
 
 class Main_Comments extends Component {
   constructor(props) {
     super(props)
     this.state = {
       comment : '',
+      comments : [],
     }
 
     this.updateComment = this.updateComment.bind(this)
+    this.addComment = this.addComment.bind(this)
   }
 
   updateComment(ev) {
     this.setState({
       comment : ev.target.value,
-    }, () => console.log(this.state))
+    })
+  }
+
+  addComment(ev) {
+    const state = {...this.state}
+    const comment = {
+      text : this.state.comment,
+      time : new Date(),
+    }
+    state.comments.push(comment)
+    state.comment = ''
+    this.setState(state)
   }
 
   render() {
     return (
-      <div>
+      <div className="comments">
         <textarea value={this.state.comment} onChange={this.updateComment} placeholder="Enter comment here"></textarea>
-        <button className="button">Submit comment!</button>
+        <button className="button" onClick={this.addComment}>Submit comment!</button>
+        {this.state.comments.map((comment, i) => <Comment comment={comment} key={i} />)}
       </div>
     )
   }
+}
+
+function Comment (props) {
+  return (
+    <div className="comment">
+      <div>{props.comment.text}</div>
+    </div>
+  )
 }
 
 export default Main_Comments
