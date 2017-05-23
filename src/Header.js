8d63e6e30fa1bb691be65d6b75ae70bb08e9fd5b
Chiranth7954 <ckishore@purdue.edu>
2017-05-23 10:10:56 -0400

    Add ten components and subcomponents

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Header_Logo from './Header_Logo'
+import Header_NavLinks from './Header_NavLinks'
 import './Header.css'
 
 class Header extends Component {
@@ -6,14 +8,8 @@ class Header extends Component {
     return (
       <div className="expanded row header">
         <div className="clearfix">
-          <div className="float-left logo">
-            <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
-          </div>
-          <div className="float-right nav-links">
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Log in</a>
-          </div>
+          <Header_Logo />
+          <Header_NavLinks />
         </div>
         <div className="headline text-center small-12 columns">
           <h1>The Dwarf Underground</h1>
--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import Main_Byline from './Main_Byline'
+import Main_Article from './Main_Article'
+import Main_Links from './Main_Links'
+import Main_Ad from './Main_Ad'
+import Main_OtherArticles from './Main_OtherArticles'
 import './Main.css'
 
 class Main extends Component {
@@ -7,80 +12,14 @@ class Main extends Component {
       <main className="expanded row">
         <div className="large-8 medium-12 columns article">
           <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
-          <div className="avatar">
-            <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
-            <div className="author-info">
-              <p className="author-name">By Domri, son of Flug</p>
-              <p className="date">on 28 April, 3018 of the Third Age</p>
-            </div>
-          </div>
-          <section className="article-body">
-            <p><span className="article-start gold">E</span>psum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
-
-            <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
-
-            <p>Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Gratuitous octopus niacin, sodium glutimate.</p>
-
-            <p className="interject">Avast! A stray goblin!</p>
-
-            <p>Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito.</p>
-
-            <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
-
-            <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
-          </section>
-          <div className="article-links">
-            <a className="article-link" href="#">
-              <i className="fa fa-comments-o"></i>
-              <span className="article-link-text">Comments</span>
-            </a>
-            <a className="article-link" href="#">
-              <i className="fa fa-share"></i>
-              <span className="article-link-text">Share Post</span>
-            </a>
-          </div>
+          <Main_Byline />
+          <Main_Article />
+          <Main_Links />
         </div>
         <aside className="large-4 medium-12 columns">
-          <div className="ad">
-            <h3>The Last Hammer You’ll Ever Need</h3>
-            <div className="ad-image">
-              <img src="https://s-media-cache-ak0.pinimg.com/originals/b4/d5/ab/b4d5ab4a82b8f7efc1adaf548df1d089.jpg" alt="hammer" />
-              <a href="#">visit sponsor</a>
-            </div>
-            <h4>Mining diamonds or crushing the skulls of your enemies, the SmashMaster 4000 does it all</h4>
-            <p className="ad-text">Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
-
-            <p className="ad-text">Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
-          </div>
+          <Main_Ad />
         </aside>
-
-        <div className="small-12 columns other-articles">
-          <h2>From around the Realm</h2>
-          <div className="small-6 medium-3 columns other-article">
-            <a href="#">
-              <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
-              <p>Single Orcs in Indianapolis</p>
-            </a>
-          </div>
-          <div className="small-6 medium-3 columns other-article">
-            <a href="#">
-              <img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain" />
-              <p>You won't believe what's under this mountain</p>
-            </a>
-          </div>
-          <div className="small-6 medium-3 columns other-article">
-            <a href="#">
-              <img src="http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" alt="gold" />
-              <p>Mine 20% more gold with One Weird Trick</p>
-            </a>
-          </div>
-          <div className="small-6 medium-3 columns other-article">
-            <a href="#">
-              <img src="http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" alt="hobbit" />
-              <p>Surprise for Indiana Hobbits born before 1999</p>
-            </a>
-          </div>
-        </div>
+        <Main_OtherArticles />
       </main>
     )
   }
